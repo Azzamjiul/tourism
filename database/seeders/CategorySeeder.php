88c7ea6bdb956b4222607549453ca342a5cf5455
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2023-02-22 21:02:42 +0700

    CategorySeeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $timestamp = date('Y-m-d H:i:s');
+
+        Category::insert([
+            ['name' => 'History & Heritage', 'created_at' => $timestamp, 'updated_at' => $timestamp],
+            ['name' => 'Museum', 'created_at' => $timestamp, 'updated_at' => $timestamp],
+            ['name' => 'Religi', 'created_at' => $timestamp, 'updated_at' => $timestamp],
+        ]);
+    }
+}
